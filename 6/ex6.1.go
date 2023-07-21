f374067fa6f44e087f231e732b050db0ef7c35e5
develjsh <develjsh@gmail.com>
2023-07-21 17:12:07 +0900

    Add 4장 변수2

---
+++ 6/ex6.1.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 3
+	var b int
+	var c = 4
+	d := 5
+
+	fmt.Println(a, b, c, d)
+
+	test()
+}
+
+func test() {
+	a := 3
+	var b float64 = 3.5
+
+	var c int = int(b)
+	d := float64(a) * b
+
+	var e int64 = 7
+	f := a * int(e)
+
+	fmt.Println(a, b, c, d, e, f)
+}
